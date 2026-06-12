3d69f8e2b2b04981cc663f9edb1e2c41d85d6700
Dennis Huang <dennis.huang03@gmail.com>
2026-06-12 19:05:11 +0700

    Rearrange the AboutHeroSection

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import './About.css';
-import AboutHero from '../components/AboutHero';
+import AboutHeroSection from '../sections/AboutUs/AboutHeroSection';
 
 const About: React.FC = () => {
   return (
-    <main className="about-page">
-      <AboutHero />
-      {/* Add more About sections here (Our Story, Team, Vision & Mission, etc.) */}
-    </main>
+      <AboutHeroSection />
   );
 };
 
---
+++ src/sections/AboutUs/AboutHeroSection.tsx
@@ -0,0 +1,10 @@
+import AboutHero from '../../components/AboutHero';
+import './AboutHeroSection.css';
+
+const AboutHeroSection: React.FC = () => {
+    return (
+        <AboutHero />
+    );
+};
+
+export default AboutHeroSection;

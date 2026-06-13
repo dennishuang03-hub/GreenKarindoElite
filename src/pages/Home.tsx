fef68ea58e6ad32b6d95ef6a05d793d230767a86
Dennis Huang <dennis.huang03@gmail.com>
2026-06-13 13:27:51 +0700

    Add Home Hero Section and fix the font on the hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 // import Navbar from "../components/Navbar";
-import Hero from "../sections/Home/Hero";
+import Hero from "../components/Hero";
 
 function Home() 
 {
---
+++ src/sections/Home/HomeHeroSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hero from '../../components/Hero';
+import './HomeHeroSection.css';
+ 
+const HomeHeroSection: React.FC = () => {
+  return <Hero />;
+};
+ 
+export default HomeHeroSection;
+ 
